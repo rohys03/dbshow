ba479085ccd8a99b446ee8645efa466e81a4caeb
노영삼(Youngsam Roh)/DB개발팀/11ST <rohys03@sk.com>
2019-09-24 20:01:10 +0900

    Apply .gitignore

---
+++ sh/startDBShow.local.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export DBSHOW_HOME=/Users/1003167/dev/IdeaProjects/dbshow
+#export JAVA_HOME=/usr/bin
+export JAVA_OPT=''
+export APP_OPT='--server.port=8081 --spring.profiles.active=stage'
+
+nohup java $JAVA_OPTS -jar $DBSHOW_HOME/build/libs/dbshow-0.0.1-SNAPSHOT.jar $APP_OPT >> $DBSHOW_HOME/log/log.out 2>&1 &
