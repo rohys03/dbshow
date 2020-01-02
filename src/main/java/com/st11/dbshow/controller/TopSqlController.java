4b67667e4bdb2e4159041a002eacbfcb03d7d685
노영삼(Youngsam Roh)/DB개발팀/11ST <rohys03@sk.com>
2020-01-02 17:40:26 +0900

    add TopSql(AWR)

--- src/main/java/com/st11/dbshow/controller/TopSqlController.java
+++ src/main/java/com/st11/dbshow/controller/TopSqlController.java
@@ -83,4 +83,61 @@ public class TopSqlController {
         return "content/topSqlDayList";
     }
 
+    @RequestMapping(value = {"topSqlDayListByAwr"})
+    public String topSqlDayListByAwr(
+            @RequestParam(value = "dbId", required = false, defaultValue = "1") final String dbId,
+            @RequestParam(value = "clctDy1", required = false) String clctDy1,
+            @RequestParam(value = "clctDy2", required = false) String clctDy2,
+            @RequestParam(value = "orderString", required = false, defaultValue = "EXEC_DIFF") final String orderString,
+            @RequestParam(value = "ascending", required = false, defaultValue = "DESC") final String ascending,
+            Model model) throws IOException, URISyntaxException {
+        final String apiMethod = "topSqlDayList";
+
+        HashMap<String, String> inParam = new HashMap<>();
+
+        SimpleDateFormat transFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar cal = Calendar.getInstance();
+
+        Date toDay = new Date();
+        cal.setTime(toDay);
+        cal.add(Calendar.DATE, -7);
+
+        String defaultClctDy1 = transFormat.format(toDay);
+        String defaultClctDy2 = transFormat.format(cal.getTime());
+
+
+        if (!isNullOrEmpty(dbId)) inParam.put("dbId", dbId.toUpperCase());
+
+        if (isNullOrEmpty(clctDy1)) clctDy1 = defaultClctDy1;
+        if (isNullOrEmpty(clctDy2)) clctDy2 = defaultClctDy2;
+
+        inParam.put("clctDy1", clctDy1.replace("-",""));
+        inParam.put("clctDy2", clctDy2.replace("-",""));
+
+
+        if (!isNullOrEmpty(orderString)) inParam.put("orderString", orderString.toUpperCase());
+        if (!isNullOrEmpty(ascending)) inParam.put("ascending", ascending.toUpperCase());
+
+        Collection<SqlAreaDiffVO> modelCollection = null;
+
+        if (!inParam.isEmpty()) {
+            modelCollection = apiService.getApiModels(apiMethod, new TypeReference<Collection<SqlAreaDiffVO>>() {
+                    }
+                    , inParam);
+        }
+
+        model.addAttribute("dbList", dbShowService.getDaDbList("Y"));
+
+        String[] orderStringList = {"EXEC_DIFF", "BGET_DIFF", "CPU_DIFF", "ELAP_DIFF"};
+        model.addAttribute("orderStringList", orderStringList);
+
+        model.addAttribute("defaultDate", LocalDateTime.now());
+        model.addAttribute("dbId", dbId);
+        model.addAttribute("clctDy1", clctDy1);
+        model.addAttribute("clctDy2", clctDy2);
+        model.addAttribute("model", modelCollection);
+
+        return "content/topSqlDayListByAwr";
+    }
+
 }
